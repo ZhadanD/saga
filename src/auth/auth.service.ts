c0cc7fa25954b7bcde535988515f083ab373f41f
Daniil <zhdv07@mail.ru>
2022-12-24 17:35:03 +0300

    Вынес логику проверки логина и пароля в отдельные методы в файле auth.service;

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,22 +25,30 @@ export class AuthService {
   }
 
   async registration(userDto: CreateUserDto): Promise<{ access_token: string }> {
-    const candidateByLogin = await this.usersService.getUserByLogin(userDto.login);
 
-    if(candidateByLogin) {
-      throw new HttpException('Недопустимый логин', HttpStatus.BAD_REQUEST);
-    }
-
-    const candidateByEmail = await this.usersService.getUserByEmail(userDto.email);
-
-    if (candidateByEmail) {
-      throw new HttpException('Пользователь с таким email уже существует', HttpStatus.BAD_REQUEST);
-    }
+    await this.loginVerification(userDto);
+    await this.emailVerification(userDto);
 
     const hashPassword = await bcrypt.hash(userDto.password, 10);
 
     const user = await this.usersService.createUser({...userDto, password: hashPassword});
     return this.generateToken(user);
+  }
+
+  async loginVerification(userDto: CreateUserDto): Promise<void> {
+    const candidateByLogin = await this.usersService.getUserByLogin(userDto.login);
+
+    if(candidateByLogin) {
+      throw new HttpException('Недопустимый логин', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async emailVerification(userDto: CreateUserDto): Promise<void> {
+    const candidateByEmail = await this.usersService.getUserByEmail(userDto.email);
+
+    if (candidateByEmail) {
+      throw new HttpException('Пользователь с таким email уже существует', HttpStatus.BAD_REQUEST);
+    }
   }
 
   async generateToken(user: any): Promise<{access_token: string}> {
